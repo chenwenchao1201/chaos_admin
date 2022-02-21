e23af7301b9b10c0de1f8f3f968bb29da10c3f94
陈文超 <305352458@qq.com>
2022-02-21 23:07:50 +0800

    项目创建

---
+++ src/main/java/com/earl/chaos/chaos_admin/ChaosAdminApplication.java
@@ -0,0 +1,13 @@
+package com.earl.chaos.chaos_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/earl/chaos/chaos_admin/ChaosAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earl.chaos.chaos_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
